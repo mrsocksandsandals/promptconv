cb8f610d68b11c0ecdd4624f35e8ba95af60c713
Brett I <mcdonalds.burger.dev@gmail.com>
2021-09-08 15:16:49 -0400

    Add some doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,24 @@
 /* promptconv - A simple tool to convert bash prompts to zsh. */
 /* ---------------------------------------------------------- */
 
+///!
+///! # promptconv
+///!
+///! A simple Bash > zsh prompt converter.
+///!
+
+/// The version number.
 pub const VERSION: &str = "0.1.3";
 
-/* MIT License
- *
- * Copyright (c) 2021 Brett I. <notronaldmcdonalds>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
-*/
-
+/// Converts a Bash prompt to a zsh prompt.
+///
+/// Example:
+/// ```rust
+/// use promptconv::convert_prompt;
+///
+/// fn main() {
+///     assert_eq!(String::from("[%n@%m %1~]%(#.#.$) "), convert_prompt(String::from("[\u@\h \W]\$ ")))
+/// }
 pub fn convert_prompt(prompt: String) -> String {
     let chars: Vec<char> = prompt.chars().collect();
     let mut n_chars: Vec<char> = vec![];
